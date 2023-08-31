486828d066e4dda6e9a0b8b2a8fe569f60ea5074
unknown <muttakinahmed871@gmail.com>
2023-08-31 17:15:55 +0600

    some change

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,6 +1,7 @@
 import logo from "../../../assets/logo.png";
+import "./Header.css";
 
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 import { useContext } from "react";
@@ -14,55 +15,51 @@ const Header = () => {
       .catch((error) => console.log(error));
   };
   return (
-    <div>
-      <div className="text-center"></div>
-      <Navbar
-        className="mt-2"
-        collapseOnSelect
-        expand="lg"
-        bg="light"
-        variant="light">
-        <Container>
-          <img className="img-fluid" src={logo} alt="" />
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mx-auto">
-              <Link className="me-2 text-decoration-none text-black" to="/">
-                Home
-              </Link>
+    <Navbar
+      className="mt-2"
+      collapseOnSelect
+      expand="lg"
+      bg="light"
+      variant="light">
+      <img className="img-fluid" src={logo} alt="" />
 
-              <Link to="/blog" className="text-decoration-none text-black">
-                Blog{" "}
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mx-auto">
+          <Link className="me-2 text-decoration-none text-black" to="/">
+            Home
+          </Link>
+
+          <Link to="/blog" className="text-decoration-none text-black">
+            Blog{" "}
+          </Link>
+        </Nav>
+        <Nav>
+          {user ? (
+            <div>
+              <img
+                className="rounded-5 me-2 mt-1 "
+                style={{ width: "40px" }}
+                data-toggle="tooltip"
+                title={user.displayName}
+                src={user.photoURL}
+              />
+              <Link>
+                <Button onClick={handleLogOut} variant="secondary">
+                  Log out
+                </Button>
               </Link>
-            </Nav>
-            <Nav>
-              {user ? (
-                <div>
-                  <img
-                    className="rounded-5 me-2 mt-1 "
-                    style={{ width: "40px" }}
-                    data-toggle="tooltip"
-                    title={user.displayName}
-                    src={user.photoURL}
-                  />
-                  <Link>
-                    <Button onClick={handleLogOut} variant="secondary">
-                      Log out
-                    </Button>
-                  </Link>
-                </div>
-              ) : (
-                <Link to="/login">
-                  <Button className="mt-1" variant="secondary">
-                    Login
-                  </Button>
-                </Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+            </div>
+          ) : (
+            <Link to="/login">
+              <Button className="mt-1 me-2" variant="secondary">
+                Login
+              </Button>
+            </Link>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
