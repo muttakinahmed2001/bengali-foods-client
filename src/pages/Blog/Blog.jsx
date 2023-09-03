498fb1143a9b70422b27305ed099fd29be565f49
unknown <muttakinahmed871@gmail.com>
2023-09-04 00:50:50 +0600

    fixed an error

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,12 +1,25 @@
 import { Container } from "react-bootstrap";
-import PDFFile from "../PDFFile";
 import PDFDownload from "../PDFDownload";
 
 const Blog = () => {
   return (
     <Container>
       <PDFDownload></PDFDownload>
-      <PDFFile></PDFFile>
+      <h1>
+        1.What is the difference between uncontrolled and controlled components?
+      </h1>
+      <h3>
+        Ans: Controlled components means the components that have their state
+        and behavior controlled by the parent component and uncontrolled
+        components means the components that manage their own state internally.
+      </h3>
+      <h1>2.How to validate React props using PropTypes?</h1>
+      <h3>Ans: At first, we have to install prop-types library.</h3>
+      <h1>3.Difference between nodejs and express js?</h1>
+      <h3>
+        Ans: Node.js is a run-time environment created to run JavaScript on the
+        server side.Express.js is a framework for Node.js.
+      </h3>
     </Container>
   );
 };
--- src/pages/PDFDownload.jsx
+++ src/pages/PDFDownload.jsx
@@ -4,15 +4,17 @@ import { Spinner } from "react-bootstrap";
 
 const PDFDownload = () => {
   return (
-    <PDFDownloadLink document={<PDFFile />} fileName="Blog">
-      {({ loading }) =>
-        loading ? (
-          <Spinner />
-        ) : (
-          <button className="btn btn-primary">Download</button>
-        )
-      }
-    </PDFDownloadLink>
+    <>
+      <PDFDownloadLink document={<PDFFile />} fileName="Blog">
+        {({ loading }) =>
+          loading ? (
+            <Spinner />
+          ) : (
+            <button className="btn btn-primary">Download</button>
+          )
+        }
+      </PDFDownloadLink>
+    </>
   );
 };
 
